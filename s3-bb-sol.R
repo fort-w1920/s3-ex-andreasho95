59281110e4013b419c8be3f21bec3c29f69d30b5
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2019-12-19 10:46:34 +0100

    Created methods for default and lists

---
+++ s3-bb-sol.R
@@ -0,0 +1,54 @@
+new_bb <- function(text) {
+  stopifnot(!is.numeric(text))
+  # replace ((consonants) (1-2 vowels) (consonants)) with
+  # ((consonants) (vowels) b (same vowels again) (consonants)):
+  match <- "([^aeiouäöüAEIOUÄÜÖ]*)([aeiouäöü]{1,2})([^aeiouäöü]*)"
+  bb_text <- gsub(pattern = match, replacement = "\\1\\2b\\2\\3", x = text)
+  
+  structure(
+    bb_text,
+    class = append(class(text),"bb") #c(class(text), "bb")
+  )
+}
+
+
+# Method dispatch
+bb <- function(x) {
+  UseMethod("bb")
+}  
+
+# Default (character vectors, matrix)
+bb.default <- function(text){
+  new_bb(text)
+}
+
+# Factors
+
+# List
+bb.list <- function(my_list){
+  bb_list <- rapply(my_list, bb.default, how = "list")
+  class(bb_list) <- append(class(my_list),"bb") #c(class(text), "bb")
+  bb_list
+}
+
+
+
+
+# Development --------------------------------------------------------------------------------------
+
+texttest <- "Bedeutet nach jedem Vokal oder Diphtong die Konsonanten..." # default
+test_vec <- strsplit(texttest, " ")[[1]] # default
+test_matrix <- matrix(test_vec, nrow = 2, ncol = 4, byrow = TRUE) # default
+test_array <- array(test_vec, dim = c(2, 2, 2)) # default
+test_list <- as.list(test_vec) # list
+
+test_listoflists <- list(as.list(test_vec), list(test_vec)) # list
+test_factor <- factor(test_vec) # factor
+test_ordered <- ordered(test_vec) # factor
+bb(texttest)
+bb(test_vec)
+bb(test_factor)
+bb(test_matrix)
+bb(test_array)
+bb(test_listoflists)
+bb(test_list)
